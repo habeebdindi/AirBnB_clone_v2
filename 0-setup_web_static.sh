7c06dc69acd2c03765157ae0a3c239c4e4ebd143
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-08 03:57:05 +0000

    Add setup config file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Script that sets up web servers for the deployment of web static
+sudo apt update
+sudo apt install nginx -y
+sudo /etc/init.d/nginx start
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+echo -e "Fake text testing nginx config\n" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown ubuntu:ubuntu -R /data
+f=/etc/nginx/sites-available/default
+echo -e "server {
+    listen 80 default_server;
+    root /usr/share/nginx/html;
+    index index.html index.htm;
+    add_header X-Served-By $hostname;
+    location /redirect_me {
+        return 301 https://www.yahoo.com;
+    }
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+}" | sudo tee $f
+sudo service nginx restart
