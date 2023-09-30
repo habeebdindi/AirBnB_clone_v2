9c5bf533ea992c0c7ca884711228818dca249d88
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-30 02:17:29 +0000

    Update file

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    __tablename__ = amenities
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship('Place',
                                    secondary=place_amenity, backref='places')
